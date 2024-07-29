d53ddbdc18b4a72223a3a5bc8af806cf5d647252
Infin-8 <nathaniel.a.vazquez@gmail.com>
2024-07-29 11:28:15 -0700

    adding dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
-import './App.css';
-import {Routes, Route, BrowserRouter} from "react-router-dom"
+import "./App.css";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Dashboard from "./views/Container/Dashboard";
 
 function App() {
   return (
-    <div>
-      Hello, World
-    </div>
-
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 

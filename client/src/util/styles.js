743f02f51208bdcdb3ec9f5218ed4ef6f9172a0f
Infin-8 <nathaniel.a.vazquez@gmail.com>
2024-07-30 11:39:13 -0700

    nav updates

--- client/src/util/styles.js
+++ client/src/util/styles.js
@@ -1,19 +1,32 @@
 const styles = {
+  gridContainer: () => ({
+    display: "grid",
+    gridTemplateRows: ".5fr 1fr 5fr",
+    height: "100vh",
+  }),
+  navContainer: () => ({
+    display: "grid",
+    gridTemplateColumns: ".5fr repeat(6, 1fr) .25fr repeat(2, 1.5fr) 2fr",
+    gap: "5px",
+    height: "100%",
+    width: "75vw",
+    justifyContent: "center",
+    alignItems: "center",
+  }),
+  navFlexUnits: (i = false, logo = false) => ({
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "70%",
+    color: i ? "white" : "rgba(4, 209, 246, 1)",
+    backgroundColor: "black",
+    fontWeight: "bolder",
+    fontSize: logo ? "50px" : "15px",
+    position: logo ? "relative" : null,
+    top: logo ? "10px" : null,
+    borderRadius: "3px"
+  }),
+};
 
-    navContainer: () => ({
-        display: "grid",
-        gridTemplateColumns: ".5fr repeat(6, 1fr) .25fr repeat(2, 1fr) 2fr",
-        height: "10vh"
-    }),
-    navFlexUnits: () => ({
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100%",
-        border: "1px solid red"
-
-    })
-}
-
-export default styles
+export default styles;
--- client/src/views/Container/Home.jsx
+++ client/src/views/Container/Home.jsx
@@ -1,18 +1,22 @@
-import Nav from "../Presentational/Nav"
-
-
+import Nav from "../Presentational/Nav";
+import styles from "../../util/styles";
 
 function Home() {
-
-
-
-    return (
-        <>
-        {<Nav />}
-
-        gifs will display here
-        </>
-    )
+    
+  return (
+    <div style={styles.gridContainer()}>
+      <div
+        className="main-grid unit"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Nav />
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
